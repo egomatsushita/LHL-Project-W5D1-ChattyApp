16a7442538e3ac98ddbf047b5d159278d0617205
egomatsushita <eduardo.go.rj@gmail.com>
2017-07-26 21:21:18 +0000

    Create getUsername function

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,21 +3,29 @@ import React, {Component} from 'react';
 class ChatBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      user: "",
+      content: ""
+    }
   }
 
   handleKeyPress(e) {
     if (e.key === 'Enter') {
-      let newMessage = { username: this.textInput.value, content: e.target.value};
-      this.props.message(newMessage);
+      let newMessage = { username: this.state.user, content: e.target.value};
+      this.state.content = this.props.message(newMessage);
       e.target.value = ""
     }
-  };
+  }
+
+  getUsername(e) {
+    this.state.user = e.target.value;
+    }
 
   render() {
     console.log("Rendering <ChatBar/>");
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" type="text" placeholder="Your Name (Optional)" defaultValue={this.props.username} ref={(input) => {this.textInput = input;}} />
+        <input className="chatbar-username" type="text" placeholder="Your Name (Optional)" defaultValue={this.props.username} onBlur={this.getUsername.bind(this)} />
         <input className="chatbar-message" type="text" placeholder="Type a message and hit ENTER" onKeyPress={this.handleKeyPress.bind(this)} />
       </footer>
     );
